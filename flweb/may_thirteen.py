b89be2b7609c4f4ac1bd81c4245a5b01c30a6693
steven <luyihsien@gmail.com>
2019-05-13 22:34:02 +0800

    'try'

--- flweb/may_thirteen.py
+++ flweb/may_thirteen.py
@@ -4,4 +4,18 @@
 #ftboy不像__init__.py會執行 除非from testss import ftboy
 #from testss.ftboy import girls才可以引入其func
 # 若為 from testss import girls則error
-from testss import ftboy
+#from testss import hello_world#還是會將__init__.py執行一次，不會因引入function就停止
+import testss
+#hello_world()#NameError: name 'hello_world' is not defined
+#import testss.hello_word#ModuleNotFoundError: No module named 'testss.hello_word'
+#猜測:若無from .. import ... 雖有執行def...但直譯過去就過去了，不會特別保存
+#WHY不circular import #pt2#猜測:本來我import裡面無論什麼東西都會先執行__init__py故此另外處理
+#from testss import ftgirls
+#from testss import ftboy
+#結果
+#abcdefg#好像在__init__py的只執行一次的樣子
+#girls
+#boys
+#from testss import ftgirls
+#abcdefg
+#girls
--- flweb/testss/ftboy.py
+++ flweb/testss/ftboy.py
@@ -1,3 +1,5 @@
+#import ftgirls#以為同個資料夾下會import的到module但其實不會
+from testss import ftgirls#import的會先執行
 print('boys')
 def girls():
-    print('girls')
+    print('boys')
