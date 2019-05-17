31227da180efd922b2ab79bd10b88d8061245494
steven <luyihsien@gmail.com>
2019-05-17 16:59:33 +0800

    'try'

--- pretest2hr/part2of2_webpage/part2of2_webpage.py
+++ pretest2hr/part2of2_webpage/part2of2_webpage.py
@@ -1,5 +1,5 @@
-#啟動直譯自己去自己的本地端網頁就能看到網頁
-# http://127.0.0.1:5000/
+#啟動直譯，本地端網頁就能看到網頁
+#網址為http://127.0.0.1:5000/
 from flask import Flask
 import requests
 res = requests.get('http://wiki.python.org.tw/The%20Zen%20Of%20Python')
