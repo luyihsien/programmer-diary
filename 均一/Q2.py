8b2c5656e82411483aaafe702ff5a4eeb9e2e283
steven <luyihsien@gmail.com>
2019-05-20 21:26:20 +0800

    'try'

--- 均一/Q2.py
+++ 均一/Q2.py
@@ -1,13 +1,11 @@
 def calculate(n):
-    a=list(range(1,n+1))
-    print(a)
-    i=0
-    while a[i]!=n:
-        if a[i]%3==0 or a[i]%5==0:
-            a.remove(a[i])
-        if a[i]%15==0:
-            a.insert(i,a[i])
-        i=i+1
-    print(a)
-    return len(a)
+    a=(range(1,n+1))
+    l=[]
+    for i in a:
+        if i%3!=0 and i%5!=0:
+            l.append(i)
+        if i%15==0:
+            l.append(i)
+    print(l)
+    return len(l)
 print(calculate(15))
