3daf3d3b5f6b1da852f4635ffe9ec0033300850f
steven <luyihsien@gmail.com>
2019-06-28 18:04:32 +0800

    ultimate lesson 33

--- flweb/food/app.py
+++ flweb/food/app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,g,request
 import sqlite3
 app=Flask(__name__)
 def connect_db():
-    sql=sqlite3.connect('data.db')
+    sql=sqlite3.connect('food_log.db')
     sql.row_factory=sqlite3.Row
     return sql
 def get_db():
@@ -22,7 +22,7 @@ def view():
     return render_template('day.html')
 @app.route('/food',methods=['GET','POST'])
 def food():
-    print(dir(g))
+    print(dir(g))#print兩次表示運行了兩次 一次URL的get一次html的action url_for的jinjia語法 POST請求#['__class__', '__contains__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'get', 'pop', 'setdefault']
     if request.method=='POST':
         name=request.form['food-name']
         protein=int(request.form['protein'])
@@ -30,7 +30,7 @@ def food():
         fat=int(request.form['fat'])
         calories=protein*4+carbohydrates*4+fat*9
         db=get_db()
-        db.execute("insert into food(name,protein,carbohydrates,fat,calories) values(?,?,?,?,?)",[name,protein,carbohydrates,fat,calories])
+        db.execute('insert into food(name,protein,carbohydrates,fat,calories) values(?,?,?,?,?)',[name,protein,carbohydrates,fat,calories])
         db.commit()
 
         return '<h1>Name:{} Protein:{} Carbs:{} Fat:{}</h1>'.format(request.form['food-name'],request.form['protein'],request.form['carbohydrates'],request.form['fat'])
